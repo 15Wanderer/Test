68266469836249c2fd753575760ca91db92fcacf
Kyle Jarrett <kyle@jarrett.org>
2019-01-10 13:15:30 -0500

    Ishan chill

--- eclipse/Part B/src/PersonB.java
+++ eclipse/Part B/src/PersonB.java
@@ -1,19 +1,18 @@
-package eclipse;
 
 import java.util.Scanner;
 
 public class PersonB 
 {
-	public static void main( strs[])
+	public static void main(String args[])
 	{
 		System.out.println("Please enter your name?");
 		Scanner scan = new Scanner(System.in);
 		String name = scan.next();
-		String out;
+		String out = "";
 		for (int i = name.length() - 1; i >= 0; i--)
 		{
 			out += name.substring(i, i + 1);
 		}
-		System.out.println(out)
+		System.out.println(out);
 	}
 }
